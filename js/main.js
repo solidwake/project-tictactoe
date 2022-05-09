88e6469ae4833e257502560d7f5f2b3684bd9092
Idris Westbrook <westbrook.idris@gmail.com>
2022-05-09 15:36:16 -0400

    add x and o functionality

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@ import GameView from './view.js';
 
 let game = new Game();
 let gameView = new GameView();
-gameView.updateBoard(game);
 
-
+let tiles = document.querySelectorAll('.board-tile');
+tiles.forEach((tile) => {
+    tile.addEventListener('click', () => {
+        onTileClick(tile.dataset.index);
+    })
+})
+function onTileClick(i) {
+    game.makeMove(i);
+    gameView.updateBoard(game);
+    game.nextTurn();
+}
