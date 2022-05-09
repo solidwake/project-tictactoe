4acf46534ba118b56f114d55959aa7150a17eeaa
Idris Westbrook <westbrook.idris@gmail.com>
2022-05-09 15:57:57 -0400

    add winning combinations

--- js/game.js
+++ js/game.js
@@ -5,13 +5,32 @@ export default class Game {
         this.turn = 'X'
     }
     nextTurn() {
-        if (this.turn == 'X') {
+        if(this.turn == 'X') {
             this.turn = 'O';
         } else {
             this.turn = 'X';
         }
     }
     makeMove(i) {
+        if(this.board[i]) {
+            return;
+        }
         this.board[i] = this.turn;
+        this.findWinningCombinations();
+    }
+    findWinningCombinations() {
+        const winningCombinations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [6,4,2]
+        ];
+        for(const combination of winningCombinations) {
+
+        }
     }
 }
