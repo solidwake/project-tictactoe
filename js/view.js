ae8b8b8827fdb5adefc6613d6ce98e067f905464
Idris Westbrook <westbrook.idris@gmail.com>
2022-05-09 15:58:15 -0400

    add winning combinations

--- js/view.js
+++ js/view.js
@@ -3,7 +3,7 @@ export default class GameView {
 
     }
     updateBoard(game) {
-        for (let i = 0; i < game.board.length; i++) {
+        for(let i = 0; i < game.board.length; i++) {
             const tile = document.querySelector(`.board-tile[data-index='${i}']`);
             tile.textContent = game.board[i];
         }
