13b7a1360cbea1919af940cc7eb5dfa961b77fe8
Idris Westbrook <westbrook.idris@gmail.com>
2022-05-10 10:05:06 -0400

    add ux elements for complete game

--- js/view.js
+++ js/view.js
@@ -1,14 +1,18 @@
 export default class GameView {
-    constructor() {
-
-    }
     updateBoard(game) {
         this.updateTurn(game);
+        const winningCombination = game.findWinningCombinations();
         for(let i = 0; i < game.board.length; i++) {
             const tile = document.querySelector(`.board-tile[data-index='${i}']`);
 
+            tile.classList.remove('winning-tiles');
+
             let tileMarker = game.board[i] == 'X' ? 'tile-x' : 'tile-o';
             tile.innerHTML = `<span class="${tileMarker}">${game.board[i] ? game.board[i] : ""}</span>`
+
+            if(winningCombination && winningCombination.includes(i)) {
+                tile.classList.add('winning-tiles');
+            }
         }
     }
     updateTurn(game) {
