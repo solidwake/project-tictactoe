70b947ee378993570732d65bb85a886833b2a2b9
Idris Westbrook <westbrook.idris@gmail.com>
2022-05-10 09:30:41 -0400

    add ux elements

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ import GameView from './view.js';
 let game = new Game();
 let gameView = new GameView();
 
+document.querySelector('.restart').addEventListener('click', () => {
+    restartGame();
+})
+
 let tiles = document.querySelectorAll('.board-tile');
 tiles.forEach((tile) => {
     tile.addEventListener('click', () => {
@@ -14,3 +18,8 @@ function onTileClick(i) {
     game.makeMove(i);
     gameView.updateBoard(game);
 }
+function restartGame() {
+    game = new Game();
+    gameView.updateBoard(game);
+}
+gameView.updateBoard(game);
--- js/view.js
+++ js/view.js
@@ -3,9 +3,22 @@ export default class GameView {
 
     }
     updateBoard(game) {
+        this.updateTurn(game);
         for(let i = 0; i < game.board.length; i++) {
             const tile = document.querySelector(`.board-tile[data-index='${i}']`);
             tile.textContent = game.board[i];
+        }
+    }
+    updateTurn(game) {
+        let playerX = document.querySelector('.player-x');
+        let playerO = document.querySelector('.player-o');
+        playerX.classList.remove('active');
+        playerO.classList.remove('active');
+
+        if (game.turn == 'X') {
+            playerX.classList.add('active');
+        } else {
+            playerO.classList.add('active');
         }
     }
 }
